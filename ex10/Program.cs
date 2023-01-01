f4f394417787d0d3fc6257b57674e1590307b223
Anastasia <Lari_an@bk.ru>
2023-01-01 20:28:30 +0300

    задача 13

--- ex10/Program.cs
+++ ex10/Program.cs
@@ -1,16 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-// Задача 13
-//Напишите программу, которая принимает на вход трёхзначное число 
-//и на выходе показывает вторую цифру этого числа.
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
 
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
 Console.Write("Введите трехзначное число: ");
 int a = int.Parse(Console.ReadLine()!);
-int b = a/10%10;
-Console.Write("второе число: ");
-Console.WriteLine(b);
+int b = a%10;
+if (a > 99)
+{
+    Console.Write("Третье число: ");
+    Console.WriteLine(b);
+}
+if (a < 99)
+{
+    
+    Console.WriteLine("Третьего числа нет! ");
+}
