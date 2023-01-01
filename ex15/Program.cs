a12d142432fdfec6b1fe5d1f89874fce8ccba29f
Anastasia <Lari_an@bk.ru>
2023-01-01 20:49:51 +0300

    задача 15

---
+++ ex15/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите число номер дня недели ");
+int day = int.Parse(Console.ReadLine()!);
+
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int g = 7;
+
+if(day == a)
+{
+    Console.WriteLine ("Понедельник");
+    Console.WriteLine ("Будний день");
+}
+
+if(day == b)
+{
+    Console.WriteLine ("Вторник");
+    Console.WriteLine ("Будний день");
+}
+if(day == c)
+{
+    Console.WriteLine ("Среда");
+    Console.WriteLine ("Будний день");
+}
+if(day == d)
+{
+    Console.WriteLine ("Четверг");
+    Console.WriteLine ("Будний день");
+}
+if(day == e)
+{
+    Console.WriteLine ("Пятница");
+    Console.WriteLine ("Будний день");
+}
+if(day == f)
+{
+    Console.WriteLine ("Суббота");
+    Console.WriteLine ("Урааа! Сегодня выходной!");
+}
+if(day == g)
+{
+    Console.WriteLine ("Воскресение");
+    Console.WriteLine ("Урааа! Сегодня выходной!");
+}
